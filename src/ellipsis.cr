3156798417f52cd8f0356254922233c53e7bf5c4
Adriene Kae <polyjitter@fastmail.com>
2022-07-16 19:38:21 -0400

    Some prerequisite stuff

--- src/ellipsis.cr
+++ src/ellipsis.cr
@@ -1,6 +1,9 @@
+require "libadwaita"
+
+require "./modules/prerequisites.cr"
+
 # TODO: Write documentation for `Ellipsis`
 module Ellipsis
-  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
-  
+
   # TODO: Put your code here
 end

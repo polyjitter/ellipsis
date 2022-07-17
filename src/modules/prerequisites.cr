fd9adb6a554e304133d4cdfbeb52f11450dc006a
Adriene Kae <polyjitter@fastmail.com>
2022-07-16 21:07:39 -0400

    I tried to fix it

--- src/modules/prerequisites.cr
+++ src/modules/prerequisites.cr
@@ -23,20 +23,4 @@ module Ellipsis
   end
 
   Gio.register_resource("data/com.github.polyjitter.Ellipsis.gresource.xml", "data")
-
-  WINDOW       = Gtk::Builder.new_from_resource("/com/github/polyjitter/Ellipsis/ui/compiled/window.ui")
-  WELCOME      = Gtk::Builder.new_from_resource("/com/github/polyjitter/Ellipsis/ui/compiled/welcome.ui")
-  MAIN         = Gtk::Builder.new_from_resource("/com/github/polyjitter/Ellipsis/ui/compiled/welcome.ui")
-  FILES_VIEW   = Gtk::Builder.new_from_resource("/com/github/polyjitter/Ellipsis/ui/compiled/files.ui")
-  CHANGES_VIEW = Gtk::Builder.new_from_resource("/com/github/polyjitter/Ellipsis/ui/compiled/changes.ui")
-
-  SPINNER = Gtk::Spinner.new(
-    spinning: true,
-    halign: Gtk::Align::Center,
-    width_request: 36,
-    height_request: 36,
-    visible: false
-  )
-
-  APP = Adw::Application.new("com.github.polyjitter.Ellipsis", Gio::ApplicationFlags::None)
 end
